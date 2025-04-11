f12014fc2ffc9c99930d6b8aed06ed4e2e7f4925
Kostya Betenya <betenya.kostya@mail.ru>
2025-04-11 20:40:31 +0300

    test workflow

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -2,10 +2,8 @@ import React, { StrictMode } from 'react'
 
 export const MainPage = () => {
     return (
-        <>
             <p>
                 This is text message!
             </p>
-        </>
     )
 }
