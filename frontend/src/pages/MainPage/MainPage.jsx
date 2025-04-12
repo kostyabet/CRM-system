ce285329b63283ca6b15ce7332a060d2716fb8c7
Kostya Betenya <betenya.kostya@mail.ru>
2025-04-12 17:56:02 +0300

    feat: check getaway responsibility

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,18 @@
 import React, { StrictMode } from 'react'
 
 export const MainPage = () => {
+    
+    fetch('http://localhost:5000/api/auth/test', { method: 'GET' }).then((response) => {
+        console.log('direct response', response.data);
+    })
+
+    fetch('http://localhost:5000/api/auth/test', { method: 'GET' }).then((response) => {
+        console.log('getaway response', response.data);
+    })
+
     return (
-            <p>
-                This is text message!
-            </p>
+        <p>
+            This is text message!
+        </p>
     )
 }
