d68dc44c2abe0a26e2dc90abb58efa84ea4dd9c4
Kostya Betenya <betenya.kostya@mail.ru>
2025-04-16 11:23:49 +0300

    feat: add resolve cors url

--- authorization/server.js
+++ authorization/server.js
@@ -5,8 +5,10 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
-
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 app.use('/api/auth', authRoutes);
 
 app.listen(5000, () => {
