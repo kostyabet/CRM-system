225ea5697e2ebcdf3337296262e8db0085f36cd7
Kostya Betenya <betenya.kostya@mail.ru>
2025-04-16 09:39:05 +0300

    fix: change getaway logic

--- getaway/index.js
+++ getaway/index.js
@@ -1,15 +1,26 @@
-require('dotenv').config();
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const axios = require('axios');
 
 const app = express();
+const PORT = 8080;
 
-app.use('/api/auth', createProxyMiddleware({
-  target: 'http://localhost:5000',
-  changeOrigin: true,
-  logLevel: 'debug',
-}));
+app.use('/api/auth', async (req, res) => {
+  try {
+    const response = await axios({
+        method: req.method,
+        url: 'http://localhost:5000' + req.originalUrl,
+        data: req.body,
+        headers: {
+            ...req.headers,
+            host: 'localhost:5000'
+        }
+    });
+    res.status(response.status).send(response.data);
+  } catch (error) {
+      res.status(error.response ? error.response.status : 500).send(error.message);
+  }
+});
 
-app.listen(8080, () => {
-  console.log('Gateway running on port 8080');
+app.listen(PORT, () => {
+  console.log(`Gateway running on port ${PORT}`);
 });
