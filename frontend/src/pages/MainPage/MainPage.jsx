205986bf5e790e5a92576a4fcdcb57fbb9a401ff
Kostya Betenya <betenya.kostya@mail.ru>
2025-04-16 18:32:24 +0300

    feat: create example request

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -2,9 +2,10 @@ import React, { StrictMode } from 'react'
 
 export const MainPage = () => {
     
-    fetch('http://localhost:80/api/auth/test', { method: 'GET' }).then((response) => {
-        console.log(response.data);
-    })
+    // Request example
+    // fetch('http://localhost:80/api/auth/test', { method: 'GET' }).then((response) => {
+    //     console.log(response);
+    // })
 
     return (
         <p>
