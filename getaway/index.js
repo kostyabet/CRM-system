6c41abbff972cbdcdb738b4165e119fc822a1acd
Kostya Betenya <betenya.kostya@mail.ru>
2025-04-16 10:51:24 +0300

    fix: log

--- getaway/index.js
+++ getaway/index.js
@@ -7,18 +7,17 @@ const PORT = 8080;
 app.use(express.json());
 
 app.use('/api/auth', async (req, res) => {
-  console.log(`/api/auth`);
-  console.log(req);
   try {
     const response = await axios({
         method: req.method,
-        url: 'http://localhost:5000' + req.path, // Используем req.path вместо req.originalUrl
+        url: 'http://localhost:5000' + req.originalUrl, // Используем req.path вместо req.originalUrl
         data: req.body,
         headers: {
             ...req.headers,
             host: 'localhost:5000'
         }
     });
+    console.log(response);
     res.status(response.status).send(response.data);
   } catch (error) {
     console.error('Error in proxy:', error.message); // Логирование ошибки
