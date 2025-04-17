2d8779e66784698f87c7d0b71c27b697cfb9ea5e
Kostya Betenya <betenya.kostya@mail.ru>
2025-04-17 12:37:16 +0300

    feat: create login & register auth methods

--- authorization/routes/authRoutes.js
+++ authorization/routes/authRoutes.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// router.post('/register', authController.register);
-// router.post('/login', authController.login);
+router.post('/register', authController.register);
+router.post('/login', authController.login);
 // router.post('/refresh-token', authController.refreshToken);
-
-router.get('/test', authController.test);
 
 module.exports = router;
--- authorization/server.js
+++ authorization/server.js
@@ -3,14 +3,18 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 
+// Initialize the express app and middleware
 const app = express();
 app.use(bodyParser.json());
 app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true
 }));
+
+// Import and use the auth routes
 app.use('/api/auth', authRoutes);
 
+// Start the server
 app.listen(5000, () => {
   console.log('Server running on port 5000');
 });
