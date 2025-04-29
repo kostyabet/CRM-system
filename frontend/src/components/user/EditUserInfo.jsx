9ba3c7feee50c981684d3b91a7b40f24380e997f
Kostya Betenya <betenya.kostya@mail.ru>
2025-04-29 11:43:32 +0300

    feat: create fomr

--- frontend/src/components/user/EditUserInfo.jsx
+++ frontend/src/components/user/EditUserInfo.jsx
@@ -5,24 +5,43 @@ import {
     Stack,
     Button
 } from '@mui/material';
+import { FormProvider, RHFTextField } from '~/components/hook-form';
 
 export const EditUserInfo = ({ 
     user
 }) => {
+    const UserSchema = Yup.object().shape({
+        login: Yup.string().required('Login is required'),
+        password: Yup.string().required('Password is required'),
+    });
+
+    const defaultValues = {
+        login: '',
+        password: '',
+        remember: true,
+    };
+
+    const methods = useForm({
+        defaultValues,
+        resolver: yupResolver(UserSchema),
+    });
+
     return(
-        <Stack spacing={1}>
-            <Typography variant="body1" gutterBottom>
-                Логин: {user?.login}
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-                Номер телефона: {user?.phone}
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-                Почта: {user?.email}
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-                Роль: {user?.role}
-            </Typography>
-        </Stack>
+        <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+            <Stack spacing={1}>
+                <Typography variant="body1" gutterBottom>
+                    Логин: {user?.login}
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                    Номер телефона: {user?.phone}
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                    Почта: {user?.email}
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                    Роль: {user?.role}
+                </Typography>
+            </Stack>
+        </FormProvider>
     );
 }
