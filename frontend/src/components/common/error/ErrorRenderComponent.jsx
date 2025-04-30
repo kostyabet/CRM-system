e86ddbc107c767904773e941c8b6d8d07cad5a91
Kostya Betenya <betenya.kostya@mail.ru>
2025-04-30 17:42:31 +0300

    feat: create error info view

--- frontend/src/components/common/error/ErrorRenderComponent.jsx
+++ frontend/src/components/common/error/ErrorRenderComponent.jsx
@@ -1,9 +1,62 @@
 import React from 'react';
+import { Container, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { m } from 'framer-motion';
+
+// assets
+import { MaintenanceIllustration } from '~/shared/assets';
+
+import Page from '../../Page';
+import { MotionContainer, varBounce } from '../../animate';
+
+// ----------------------------------------------------------------------
+
+const ContentStyle = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    margin: 'auto',
+    maxWidth: 480,
+    minHeight: '100vh',
+    padding: theme.spacing(12, 0),
+}));
+
+// ----------------------------------------------------------------------
 
 export default function ErrorRenderComponent({ error }) {
     return (
-        <div>
-            <p>Some error</p>
-        </div>
+        <Page title="404 Page Not Found">
+            <Container component={MotionContainer}>
+                <ContentStyle
+                    sx={{ alignItems: 'center', textAlign: 'center' }}
+                >
+                    <m.div variants={varBounce().in}>
+                        <Typography paragraph variant="h3">
+                            Извините, в течение рендера произошла ошибка
+                        </Typography>
+                    </m.div>
+
+                    <m.div variants={varBounce().in}>
+                        <Typography sx={{ color: 'text.secondary' }}>
+                            Пожалуйста, попробуйте позже или обратитесь к
+                            администратору прикрепив сообщение ниже.
+                        </Typography>
+                    </m.div>
+
+                    <m.div variants={varBounce().in}>
+                        <Typography>
+                            Сообщение: {error?.message}
+                            {error?.stack}
+                        </Typography>
+                    </m.div>
+
+                    <m.div variants={varBounce().in}>
+                        <MaintenanceIllustration
+                            sx={{ height: 260, my: { sm: 10, xs: 5 } }}
+                        />
+                    </m.div>
+                </ContentStyle>
+            </Container>
+        </Page>
     );
 }
