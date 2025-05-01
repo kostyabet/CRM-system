9ee82578781b6b03aee13702b0331d52f1762672
Kostya Betenya <betenya.kostya@mail.ru>
2025-05-01 18:10:12 +0300

    feat: create status requests

--- tasks/controllers/stateController.js
+++ tasks/controllers/stateController.js
@@ -4,17 +4,58 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 exports.add = async (req, res) => {
+    try {
+        const { RU, EN } = req.body;
 
-}
+        const existing = await State.findOne({ where: { RU, EN } });
 
-exports.change = async (req, res) => {
+        if (existing) {
+            return res.status(400).json({ message: `Такой статус уже существует.` });
+        }
 
+        const status = await State.create({
+            RU: RU,
+            EN: EN
+        });
+      
+        res.status(201).json({
+            message: 'Статус создан',
+            status: status
+        });
+    } catch(error) {
+        console.error('Ошибка добавления статуса:', error);
+        res.status(500).json({ message: 'Ошибка сервера' });
+    }
 }
 
 exports.get = async (req, res) => {
+    try {
+        const states = await State.findAll();
 
+        res.status(200).json({
+            states,
+        });
+    } catch(error) {
+        console.error('Ошибка при получении статусов:', error);
+        res.status(500).json({ message: 'Ошибка сервера' });
+    }
 }
 
 exports.delete = async (req, res) => {
+    try {
+        const { id } = req.body;
 
+        const status = await State.findOne({ where: { id } });
+
+        if (!status) {
+            return res.status(400).json({ message: `Статуса с таким id: ${id} не существует.` });
+        }
+
+        await State.destroy({ where: { id } });
+
+        return res.status(200).json({ message: `Статус с id: ${id} успешно удалён.` });
+    } catch(error) {
+        console.error('Ошибка при удалении статуса:', error);
+        res.status(500).json({ message: 'Ошибка сервера' });
+    }
 }
