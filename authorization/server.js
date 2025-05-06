fc8d5aefb54999799133439b440a09591dc4d5de
Kostya Betenya <betenya.kostya@mail.ru>
2025-05-06 20:32:53 +0300

    feat: change cors policy

--- authorization/server.js
+++ authorization/server.js
@@ -9,7 +9,16 @@ const path = require('path');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors({
-  origin: ['http://localhost:5001', 'http://localhost:4000', 'http://localhost:3000'],
+  origin: function(origin, callback) {
+    // Разрешаем доступ для localhost и IP-адресов в диапазоне 192.168.*
+    if (!origin || 
+        /^http:\/\/localhost(:\d+)?$/.test(origin) || 
+        /^http:\/\/192\.168\.\d+\.\d+(:\d+)?$/.test(origin)) {
+      callback(null, true);  // Разрешаем запросы
+    } else {
+      callback(new Error('Not allowed by CORS'));  // Отказываем всем остальным
+    }
+  },
   credentials: true
 }));
 
