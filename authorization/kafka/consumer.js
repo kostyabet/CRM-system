49dd73c1e6e7641c0d51a17e81ca168d89fec5b3
Kostya Betenya <betenya.kostya@mail.ru>
2025-05-09 18:08:35 +0300

    feat: add kafka to the project

---
+++ authorization/kafka/consumer.js
@@ -0,0 +1,33 @@
+const { checkUsersExists } = require('./../controllers/authController');
+
+const { Kafka } = require('kafkajs');
+
+const kafka = new Kafka({ brokers: ['kafka:9092'] });
+const consumer = kafka.consumer({ groupId: 'auth-service' });
+const producer = kafka.producer();
+
+async function run() {
+  await consumer.connect();
+  await producer.connect();
+  await consumer.subscribe({ topic: 'check-user' });
+
+  await consumer.run({
+    eachMessage: async ({ message }) => {
+      const { users, correlationId } = JSON.parse(message.value.toString());
+
+      const exists = await checkUsersExists(users);
+
+      await producer.send({
+        topic: 'check-user-response',
+        messages: [
+          {
+            key: correlationId,
+            value: JSON.stringify({ users, exists, correlationId })
+          }
+        ]
+      });
+    }
+  });
+}
+
+run().catch(console.error);
