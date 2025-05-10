3ee5ade00d7c185a74f060dc3f8cf8a33cc2ce17
Kostya Betenya <betenya.kostya@mail.ru>
2025-05-10 19:56:31 +0300

    fix: small copilot fixes

--- log/server.js
+++ log/server.js
@@ -27,7 +27,7 @@ app.post('/log', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, err => {
   if (err) {
     log('log_service', 'ERR', `Error starting server ${err}`);
